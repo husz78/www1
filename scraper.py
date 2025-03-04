3654df7a0416afbf18c2175322fc8dd91e09cb28
Hubert <hubertszym4@gmail.com>
2025-03-04 21:47:52 +0100

    First commit

---
+++ scraper.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+
+response = requests.get("https://www.tiobe.com/tiobe-index/")
+if response.status_code != 200:
+    response.raise_for_status()
+
+content = response.text
+soup = BeautifulSoup(content, 'html.parser')
+
+table = soup.table.tbody
+tds_with_img = table.find_all('td', class_="td-top20")
+imgs = ["www.tiobe.com" + td.img.get("src") for td in tds_with_img]
+td_languages = [td.next_sibling for td in tds_with_img]
+percentage = [language.next_sibling.text for language in td_languages]
+languages = [x.text for x in td_languages]
+
+urls = []
+for language in languages:
+    url = list(search(language, stop=1))[0]
+    urls.append(url)
+    print("Appended url")
+
+md_file = "# List of the most popular programming languages in the world"
+
+for i in range(len(languages)):
+    md_file += f"\n- # {languages[i]} ![image]({imgs[i]})"
+    md_file += f"\n     - Percentage of popularity: {percentage[i]}"
+
+
+# print(md_file)
+with open("page.md", "w") as file:
+    file.write(md_file)
+
+print("File written")
